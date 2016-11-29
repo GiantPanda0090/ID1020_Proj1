efeab0e19d286536e69378a6a500f0e15ddf8bc1
Qi Li <liqischool@gmail.com>
2016-11-29 11:04:27 +0100

    right implementation

--- src/main/java/proj1/Main.java
+++ src/main/java/proj1/Main.java
@@ -1,6 +1,7 @@
 package proj1;
 
 import lab_4.Driver;
+import se.kth.id1020.TinySearchEngineBase;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -10,7 +11,9 @@ import java.util.Scanner;
  * Created by AlstonQiLee on 2016-11-28.
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
+         TinySearchEngineBase searchEngine = new TinySearchEngine();
+         Driver.run(searchEngine);
+         }
 
-    }
 }
---
+++ src/main/java/proj1/TinySearchEngine.java
@@ -0,0 +1,24 @@
+package proj1;
+
+
+import se.kth.id1020.TinySearchEngineBase;
+import se.kth.id1020.util.Attributes;
+import se.kth.id1020.util.Document;
+import se.kth.id1020.util.Word;
+
+import java.util.List;
+
+/**
+ * Created by AlstonQiLee on 2016-11-28.
+ */
+public class TinySearchEngine implements TinySearchEngineBase{
+
+    public void insert(Word word, Attributes attributes) {
+
+
+    }
+
+    public List<Document> search(String s) {
+        return null;
+    }
+}
